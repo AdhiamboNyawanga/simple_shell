8d8ce5b995c412a0b895638725fa7d45595c0177
AERENE <99390318+AERENE@users.noreply.github.com>
2022-05-17 11:34:34 +0300

    Commit

--- strlen.c
+++ strlen.c
@@ -13,4 +13,4 @@ int _strlen(char *s)
 		return (0);
 	else
 		return (1 + _strlen(s + 1));
-}[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[i[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[D[D[D[D[D[D[i[A[A[A[C[C[C[C[i)[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[i[B[B[B[B[B[B[B[B[B[B[B
+}
