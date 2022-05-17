cef13036721aeb8226a5907ae9dc6bb3076f8674
AERENE <Mugaadhiambo19@students.ku.ac.ke>
2022-05-17 11:33:32 +0300

    Commit

---
+++ strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - function
+*
+* @s: the chain
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[i[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[D[D[D[D[D[D[i[A[A[A[C[C[C[C[i)[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[i[B[B[B[B[B[B[B[B[B[B[B
